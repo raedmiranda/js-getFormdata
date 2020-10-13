93662fcef35e3c16700e8d8360139354ef39c9cc
raedmiranda <lalo_sagitario.95@hotmail.es>
2020-10-13 00:12:12 -0500

    Revert "Mocha's test ok"
    This reverts commit fb2af02063f5172102e0f55771dfe80127dad793.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 var should = require('should'),
-    assert = require('assert').strict;
+    assert = require('assert');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
@@ -9,15 +9,19 @@ const { JSDOM } = jsdom;
 
 var getData = require('..')
 
-describe('getData test', function () {
+describe('getData', function () {
     // create some jsdom magic to allow jQuery to work
     var document = new JSDOM('<!doctype html><html><body><form><input type=text name=user value=Lorem/><input type=email name=mail value="q@w.co"/></form></body></html>'),
         window = document.window,
         $ = global.jQuery = require('jquery')(window);
 
-    it('get data as object', function () {
+    it('get data', function () {
         let oData = getData($("form"));
         oData.should.be.a.Object();
-        //var beautified = JSON.stringify(data, null, 2)
     })
 })
+
+
+var $form = $("#form_data");
+var data = getFormData($form);
+var beautified = JSON.stringify(data, null, 2)
