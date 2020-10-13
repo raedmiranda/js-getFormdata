08de138657504240a74a285160adca531a824aa5
raedmiranda <lalo_sagitario.95@hotmail.es>
2020-10-13 00:22:31 -0500

    Tested with Mocha

--- History.md
+++ History.md
@@ -1,3 +1,8 @@
 0.0.1 - October 12, 2020
 --------------------------
 🎉
+
+0.0.2 - October 12, 2020
+--------------------------
+Tested with Mocha
+Dependency with jQuery
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 'use strict';
 
-import * as $ from 'jquery'
+/**
+ * Export
+ */
+module.exports = getFormData
 
-module.exports = {
-    getFormData
-}
-
+/**
+ * Convert a jQuery object to JS object. 
+ * It operates on a jQuery collection of forms and/or form controls.
+ * 
+ * @param {Object} a - jQuery collection of forms and/or form controls
+ * @returns {Object} JS Object
+ * 
+ * @version 1.0.0  
+ * See the {@link https://github.com/raedmiranda/js-getFormdata Documentation}
+ */
 function getFormData(a) {
+    //First: Encode a set of form elements as an array of names and values.
     var r = a.serializeArray(), e = {};
-    return $.map(r, function (a, r) { e[a.name] = a.value }), e
+    //Then: Convert the name-value array to an object
+    return jQuery.map(r, function (a, r) { e[a.name] = a.value }), e
 }
