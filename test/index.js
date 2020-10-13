e63acf7d33300663b6abde7e79af911b86c980d5
raedmiranda <lalo_sagitario.95@hotmail.es>
2020-10-13 00:34:10 -0500

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var should = require('should'),
     assert = require('assert').strict;
 const jsdom = require('jsdom');
+const { beforeEach } = require('mocha');
 const { JSDOM } = jsdom;
 
 /**
@@ -10,13 +11,15 @@ const { JSDOM } = jsdom;
 var getData = require('..')
 
 describe('getData test', function () {
-    // create some jsdom magic to allow jQuery to work
-    var document = new JSDOM('<!doctype html><html><body><form><input type=text name=user value=Lorem/><input type=email name=mail value="q@w.co"/></form></body></html>'),
-        window = document.window,
-        $ = global.jQuery = require('jquery')(window);
+    beforeEach(function () {
+        // create some jsdom magic to allow jQuery to work
+        this.document = new JSDOM('<!doctype html><html><body><form><input type=text name=user value=Lorem/><input type=email name=mail value="q@w.co"/></form></body></html>');
+        this.window = this.document.window;
+        this.$ = global.jQuery = require('jquery')(this.window);
+    })
 
     it('get data as object', function () {
-        let oData = getData($("form"));
+        let oData = getData(this.$("form"));
         oData.should.be.a.Object();
         //var beautified = JSON.stringify(data, null, 2)
     })
